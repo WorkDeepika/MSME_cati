1b89173003705cd11da9b1560047a0e655c0a610
deepika8383 <jaindeepika8383@gmail.com>
2025-05-13 19:19:16 +0530

    jjhvj

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express'); 
+const connectDB = require('./services/db/mongo');
+const cors = require('cors');
+const { createObjectCsvWriter } = require('csv-writer');
+const path = require('path');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const routes= require('./Routes/main')
+const createTable = require("./services/db/neon");
+const app = express(); 
+app.use(express.json()); 
+app.use(cors({
+    origin: "*",
+    methods: ["POST", "GET", "PUT"],
+    credentials: true
+}));
+
+
+dotenv.config();
+connectDB();
+createTable();
+app.use('/', routes);
+
+app.listen(3001, () => {
+    console.log(`Server running on port 3000`);
+});
