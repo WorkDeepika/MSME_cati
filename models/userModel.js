ec3c9bb581ebeefe2f08f5f17a146e710cdd58c6
deepika8383 <jaindeepika8383@gmail.com>
2025-05-13 20:27:26 +0530

    user

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,18 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 
-// const userSchema = new mongoose.Schema({
-//   email: { 
-//     type: mongoose.Schema.Types.Mixed , default: null
-//   },
-//   password: { type: mongoose.Schema.Types.String, required: true }
-// }, {timestamps: true});
+const userSchema = new mongoose.Schema({
+  email: { 
+    type: mongoose.Schema.Types.Mixed , default: null
+  },
+  password: { type: mongoose.Schema.Types.String, required: true }
+}, {timestamps: true});
 
 
-// const User1 = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
-// module.exports = {
-//     User1
-// };
+module.exports = {
+    User
+};
 
 
