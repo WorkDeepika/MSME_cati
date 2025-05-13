cbcef58a8289738927ec930c4e5e94e2e97a959a
deepika8383 <jaindeepika8383@gmail.com>
2025-05-13 20:35:47 +0530

    user

--- Routes/main.js
+++ Routes/main.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const {welcome}=require("../controllers/wecomeController")
 // const { getAudioUrlController }= require("../controllers/audioController")
-const { getImageUrl }= require("../controllers/addImageController")
-const { loginController,  }= require("../controllers/loginController")
-const {putData, addUser, createOrUpdateProject, }= require("../controllers/addDataController")
-const {exportDataToXLSX }=  require("../controllers/getCsvController")
+// const { getImageUrl }= require("../controllers/addImageController")
+// const { loginController,  }= require("../controllers/loginController")
+const {putData, addUser, }= require("../controllers/addDataController")
+// const {exportDataToXLSX }=  require("../controllers/getCsvController")
 const multer = require('multer');
 const path = require('path');
-const { getDateTime } = require('../controllers/getTimeDate');
-const { genralInfo,getAllProjects } = require('../controllers/dashBoredController');
-const { filterData } = require('../controllers/filterdataController');
-const { tempAddData} = require('../controllers/tempAdd')
+// const { getDateTime } = require('../controllers/getTimeDate');
+// const { genralInfo,getAllProjects } = require('../controllers/dashBoredController');
+// const { filterData } = require('../controllers/filterdataController');
+// const { tempAddData} = require('../controllers/tempAdd')
 // const { getInfo } = require('../controllers/dashboredController');
 
 // Configure multer storage
@@ -22,7 +22,7 @@ const router = express.Router();
 router.get('/', welcome)
 router.post('/add-data',putData);
 // router.post('/temp-add-data', tempAddData);
-// router.post('/add-user',addUser);
+router.post('/add-user',addUser);
 // router.post('/get-xlsx',filterData);
 // router.post('/addOrUpdateProject',createOrUpdateProject);
 // router.get('/download-xlsx',exportDataToXLSX)
